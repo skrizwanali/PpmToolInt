f39a515e2015eb7538eec3f9c23d1ea9b54378f7
Rizwan Ali <Rizwan.Ali@synechron.com>
2020-09-12 11:26:53 +0530

    branch3

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
@@ -1,7 +1,11 @@
 package io.agileintelligence.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import javax.persistence.*;
 import java.util.Date;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Project {
@@ -9,13 +13,21 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "Project name is required.")
     private String projectName;
+    @NotBlank(message = "Project id is required.")
+    @Size(min = 4, max = 5, message = "Please use 4 to 5 characters.")
+    @Column(updatable = false, unique = false)
     private String projectIdentifier;
+    @NotBlank(message = "Project description is required.")
     private String description;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
-
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_at;
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_at;
 
     public Project() {
