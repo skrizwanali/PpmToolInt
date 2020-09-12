3b0fb63a98294c90d02d83e47e7798820ee8d2e8
Rizwan Ali <Rizwan.Ali@synechron.com>
2020-09-12 18:01:55 +0530

    branch9

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -41,4 +41,9 @@ public class ProjectController {
 		Project project = projectService.findProjectByIdentifier(projectId);
 		return new ResponseEntity<>(project, HttpStatus.OK);
 	}
+
+	@GetMapping("/all")
+	public Iterable<Project> findAllProjects(){
+		return projectService.findAllProjects();
+	}
 }
