450e89b283a70c0d80dddca067e8a7986cec7971
Rizwan Ali <Rizwan.Ali@synechron.com>
2020-09-20 08:36:44 +0530

    branch11-12-13

---
+++ ppmtool-react-client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Dashboard from "./components/Dashboard";
+import "./App.css";
+import Header from "./components/Layout/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
