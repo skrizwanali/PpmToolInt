6557ecdd72fa3382d339f66ff5fa56cd654a7daf
Rizwan Ali <Rizwan.Ali@synechron.com>
2020-09-24 11:16:54 +0530

    branch18

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
+import AddProject from "./components/Project/AddProject";
+import store from "./store";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import AddProject from "./components/Project/AddProject";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/addProject" component={AddProject} />
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
